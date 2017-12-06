6da7627bd97af6180a33023081208ba2ac876fd9
cmcx <cms@dock.in-berlin.de>
2017-12-06 17:15:28 +0100

    show.php reihenfolge

--- items/show.php
+++ items/show.php
@@ -2,37 +2,39 @@
 <div id="primary">
     <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
 
+    <!-- plugin hook (place universal viewer here) -->
+       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+    <!-- The following prints a citation for this item. -->
+    <div id="item-citation" class="element">
+        <h4><?php echo __('Citation'); ?></h4>
+        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+    </div>
+
+
     <!-- Items metadata -->
     <div id="item-metadata">
         <?php echo all_element_texts('item'); ?>
     </div>
 
-    <h3><?php echo __('Files'); ?></h3>
+    <h4><?php echo __('Files'); ?></h4>
     <div id="item-images">
          <?php echo files_for_item(); ?>
     </div>
 
    <?php if(metadata('item','Collection Name')): ?>
       <div id="collection" class="element">
-        <h3><?php echo __('Collection'); ?></h3>
+        <h4><?php echo __('Collection'); ?></h4>
         <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
       </div>
    <?php endif; ?>
-
      <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata('item','has tags')): ?>
     <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
+        <h4><?php echo __('Tags'); ?></h4>
         <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif;?>
-
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
-    </div>
-       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
     <ul class="item-pagination navigation">
