d35510a55dfc8c7dce87286a7695724b0e031e12
cmcx <cms@dock.in-berlin.de>
2018-01-24 20:27:34 +0100

    changed base font size back to 12pt

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
-sshpass -p $(cat ~/konfig/mbc/pass) rsync -rv --exclude='push.sh' * cms@max-bense.ub.uni-stuttgart.de:/var/www/mbc/themes/stuttgart/
+sshpass -p $(cat ~/konfig/mbc/pass) rsync -rv --exclude='push.sh' --exclude='pushl.sh' --exclude='.sass-cache' * cms@max-bense.ub.uni-stuttgart.de:/var/www/mbc/themes/stuttgart/
 
